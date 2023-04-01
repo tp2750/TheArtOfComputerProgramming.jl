17012a73bcb5f24ccc7022012feff8d99740072a
Documenter.jl <documenter@juliadocs.github.io>
2023-04-01 15:19:00 +0000

    build based on caeb09b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TheArtOfComputerProgramming","category":"page"},{"location":"#TheArtOfComputerProgramming","page":"Home","title":"TheArtOfComputerProgramming","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TheArtOfComputerProgramming.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TheArtOfComputerProgramming]","category":"page"}]
+}
